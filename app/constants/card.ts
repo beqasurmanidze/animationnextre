b0cf2b76b3ff4edbe5c7343fa5471c8f3757056b
Your Name <beqasurmana2900@gmail.com>
2024-11-08 20:00:05 +0400

    adding site core

---
+++ app/constants/card.ts
@@ -0,0 +1,38 @@
+export type CardType = {
+  title: string;
+  id: number;
+  image: string;
+};
+
+export const cards = [
+  {
+    title: "title 1",
+    id: 1,
+    image: "./assets/images/image1.png",
+  },
+  {
+    title: "title 1",
+    id: 2,
+    image: "./assets/images/image1.png",
+  },
+  {
+    title: "title 1",
+    id: 3,
+    image: "./assets/images/image1.png",
+  },
+  {
+    title: "title 1",
+    id: 4,
+    image: "./assets/images/image1.png",
+  },
+  {
+    title: "title 1",
+    id: 5,
+    image: "./assets/images/image1.png",
+  },
+  {
+    title: "title 1",
+    id: 6,
+    image: "./assets/images/image1.png",
+  },
+];
